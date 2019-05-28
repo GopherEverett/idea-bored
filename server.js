9b85794a16b445cac63f3d35dd1254a9442ee822
John Jacobs <john.a.jacobs1973@gmail.com>
2019-05-28 09:16:43 -0400

    server set up initial

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Doin It on port" + PORT);
+})
