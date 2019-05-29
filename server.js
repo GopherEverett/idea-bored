d59093923914e887ed149d5ba968a3cd6e7f7f17
John Jacobs <john.a.jacobs1973@gmail.com>
2019-05-29 07:35:08 -0400

    first deploy

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/client/build/'));
 app.use(express.json());
-app.get('/', (req,res) => {
-    res.send('Hello World!')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
 })
 
 const PORT = process.env.PORT || 3001;
